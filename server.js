2c08b06c1b8fa3c16aec01e1e00c219b594f7718
FrameTee29 <teeraphat123@hotmail.com>
2020-01-22 23:22:44 +0700

    Edit Random number

--- server.js
+++ server.js
@@ -3,20 +3,23 @@ var HOST = '127.0.0.1';
 var PORT = 6969;
 var i= 0;
 var server = net.createServer();
+var guess=Math.floor(Math.random() * 21);
 server.listen(PORT, HOST);
 server.on('connection', (sock) => {
     console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
     
     sock.on('data', (data) => {
         console.log('DATA ' + sock.remoteAddress + ':=> ' + data);
+        console.log("Guess" + guess);
         console.log(i++);
         if (i == 1 || i==0) {
             sock.write(' OK ');
             console.log(i++);
         }
         if(i+1 !== 8){
-            if (data == Math.floor(Math.random() * 21)) {
+            if (data == guess) {
                 i=0;
+                guess=Math.floor(Math.random() * 21);
                 sock.write('BINGO');
             }
             else {
